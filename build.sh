969b744eaf6a23712c4d1bc0545f689e203fe40d
jprodrigues644 <jprodrigues644@gmail.com>
2023-06-12 16:48:24 +0200

    scrript

---
+++ build.sh
@@ -0,0 +1,2 @@
+npm install dotenv
+node script.js
---
+++ script.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+
+
+const USER_NAME = process.env.USER_NAME;
+const PASSWORD = process.env.PASSWORD;
+const MAP_KEY = process.env.MAP_KEY;
+
+console.log("User Name:", USER_NAME);
+console.log("Password:", PASSWORD);
+console.log("Map Key:", MAP_KEY);
