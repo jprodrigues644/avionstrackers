9961712bcb3dbe2067c83c350c6f692239cfdac1
jprodrigues644 <jprodrigues644@gmail.com>
2023-06-21 10:52:19 +0200

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,43 @@
-const Dotenv = require('dotenv-webpack');
+/* eslint-disable import/no-anonymous-default-export */
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import Dotenv from 'dotenv-webpack';
 
-module.exports = {
-  // ...other webpack configuration options
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
     new Dotenv(),
-    // ...other plugins
   ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+  },
 };
